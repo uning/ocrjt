5cf2da4cb974e2969a0f40c90810053f0e1b4cc6
tingkun <tingkun@ruqiju.cn>
2023-07-18 10:24:28 +0800

    增加历史

--- ocr.js
+++ ocr.js
@@ -13,9 +13,8 @@ XLSX.set_fs(fs);
 
 
 
-const args = process.argv.slice(2);
 const helpStr = `
-会在output目录下,将截图目录扁平化建立目录,将识别结果写入该目录下ocr.xlst,ocr.csv
+会在输出目录下,将截图目录扁平化建立目录,将识别结果写入该目录下ocr.xlst,ocr.csv
 出错的文件写入 ocr.csv.err
 node ${__filename} 713龚洋/test bd pt 
 结果写入  ${__dirname}/output/713龚洋_test
@@ -24,7 +23,7 @@ node ${__filename} 713龚洋/test bd pt
   平台          ---   bd(百度) tx（腾讯) (默认bd)
   方法          ---   acc(高精度) pt(普通)  ptl(标准+位置) accl(高精+位置) (默认pt)
   输出目录       ---   输出结果目录（默认 ./output）
-  处理子目录层级  ---   默认 1
+  处理子目录层级  ---   默认 2
 
 `;
 
@@ -50,7 +49,7 @@ async function wait(ms) {
 function mkdirp(directory) {
   try {
     fs.mkdirSync(directory, { recursive: true });
-    console.log(directory,'目录已创建或已存在');
+    console.log(directory, '目录已创建或已存在');
   } catch (err) {
     if (!err.code === 'EEXIST') {
       console.error('无法创建目录:', err);
@@ -64,20 +63,20 @@ function mkdirp(directory) {
 function readImageFiles(dir, results, maxDepth = 2) {
   const queue = [{ dir, depth: 0 }];
   while (queue.length > 0) {
-      const { dir, depth } = queue.shift();
-      if (depth > maxDepth) continue;
-      const files = fs.readdirSync(dir);
-      files.forEach(file => {
-          const filePath = path.join(dir, file);
-          const stat = fs.statSync(filePath);
-          if (stat && stat.isDirectory()) {
-              queue.push({ dir: filePath, depth: depth + 1 });
-          } else {
-              if (file.toLocaleLowerCase().match(/\.(jpg|jpeg|png|gif)$/)) {
-                  results.push(filePath);
-              }
-          }
-      });
+    const { dir, depth } = queue.shift();
+    if (depth > maxDepth) continue;
+    const files = fs.readdirSync(dir);
+    files.forEach(file => {
+      const filePath = path.join(dir, file);
+      const stat = fs.statSync(filePath);
+      if (stat && stat.isDirectory()) {
+        queue.push({ dir: filePath, depth: depth + 1 });
+      } else {
+        if (file.toLocaleLowerCase().match(/\.(jpg|jpeg|png|gif)$/)) {
+          results.push(filePath);
+        }
+      }
+    });
   }
 }
 
@@ -110,6 +109,7 @@ async function readKeys(file) {
   qtsbxx: '13:57|23.6|HD|4G|5G|56|KB/s|..l|ol|订单详情|待发货|何宇轩|15698470326|河南省郑州市管城回族区河南省郑州市管城区陇海|订单编号|11224710003333|下单时间|2023-07-12 13:54:03|商品总价|￥1408|运费|￥0|实付款￥1408|申请售后|联系客服|前往首页|'
 */
 async function processAll() {
+  const args = process.argv.slice(2);
   const imgdir = args[0];
   if (!fs.existsSync(imgdir)) {
     console.log(`截图目录 ${imgdir} 不存在`);
@@ -118,72 +118,74 @@ async function processAll() {
 
   const platform = args[1] || 'bd';
   const method = args[2] || 'pt';
-  const outputDir = args[3] || path.join(__dirname, 'output');
-  const maxDepth = args[4] || 1;
+  //const outputDir = args[3] || path.join(__dirname, 'output');
+  const outputDir = args[3] || path.join(imgdir, 'output');;
+
+  const maxDepth = args[4] || 2;
 
 
 
 
   const fn = imgdir.replace(/\./g, '').replace(/\//g, '_');
   let sourceDirectory = imgdir; // 指定目录的路径
-  if (path.isAbsolute(sourceDirectory))
-    sourceDirectory = path.path.resolve(imgdir);
+  if (!path.isAbsolute(sourceDirectory))
+    sourceDirectory = path.resolve(imgdir);
 
   mkdirp(outputDir);
 
-  const processedResultFile = path.join(outputDir, fn + '.processed.txt'); // 输出文件的路径
+  const processedResultFile = path.join(outputDir, '.' + fn + '.processed.txt'); // 输出文件的路径
 
   const outputFilePath = path.join(outputDir, fn + 'ocr.csv'); // 输出文件的路径
   const outputXlsFilePath = path.join(outputDir, fn + 'ocr.xlsx'); // 输出文件的路径
 
-  const csvhead = '文件,日期,下单人,手机号,地址,实付款,订单号,下单时间,商品总价,商品名,其他信息' + "\n";
+  const csvhead = '文件,日期,备注,下单人,手机号,地址,实付款,订单号,下单时间,商品总价,商品名,其他信息' + "\n";
 
   fs.writeFileSync(outputFilePath, csvhead);
   fs.writeFileSync(outputFilePath + '.err', '');
-  
+
+
+  //读取已经处理的文件
   let processedFiles = {}
-
-  try{
-   processedFiles = await readKeys(processedResultFile);
-  }catch(e){
-
+  try {
+    processedFiles = await readKeys(processedResultFile);
+  } catch (e) {
   }
-  console.log('processedFiles:',processedFiles);
+  console.log('processedFiles:', processedFiles);
 
 
 
 
 
-  /*
-  // 读取指定目录下的所有文件
-  const files = fs.readdirSync(sourceDirectory);
-  const imageFiles = files.filter(file => {
-    const ext = path.extname(file).toLowerCase();
-    return ext === '.jpg' || ext === '.jpeg' || ext === '.png' || ext === '.gif';
-  });
-  */
-  const imageFiles  = [];
-  readImageFiles(sourceDirectory,imageFiles);
-  console.log('总共 ' +imageFiles.length+' 个文件');
+  const imageFiles = [];
+  readImageFiles(sourceDirectory, imageFiles,maxDepth);
+  console.log('procees ',sourceDirectory,': 总共 ' + imageFiles.length + ' 个文件');
   // return;
   // 遍历每个文件
   let i = 0;
 
   const wsData = [];
-  wsData.push(['文件(可点击打开)', '日期', '下单人', '手机号', '地址', '实付款', '订单号', '下单时间', '商品总价', '商品名', '其他信息']);
+  wsData.push(['文件(可点击打开)', '日期','备注', '下单人', '手机号', '地址', '实付款', '订单号', '下单时间', '商品总价', '商品名', '其他信息']);
   for (i = 0; i < imageFiles.length; ++i) {
     //const filePath = path.join(sourceDirectory, imageFiles[i]);
-    const filePath =  imageFiles[i];
+    const filePath = imageFiles[i];
+    if (processedFiles[filePath]) {
+      console.log('processed :', filePath);
+      continue;
+    }
+
     try {
       const ret = await OCR[platform].general(filePath, method);
 
       ret.filename = path.relative(outputDir, filePath);
-      const csvData = `${ret.filename},${ret.rq},${ret.xdr},${ret.sjh},${ret.shdz},${ret.sfk},${ret.ddh1},${ret.xdsj},${ret.spzj},${ret.cpm},${ret.qtsbxx}\n`;
+      const csvData = `${ret.filename},${ret.rq},,${ret.xdr},${ret.sjh},${ret.shdz},${ret.sfk},${ret.ddh1},${ret.xdsj},${ret.spzj},${ret.cpm},${ret.qtsbxx}\n`;
       fs.appendFileSync(outputFilePath, csvData);
-      wsData.push([ret.filename, ret.rq, ret.xdr, ret.sjh, ret.shdz, ret.sfk, ret.ddh1, ret.xdsj, ret.spzj, ret.cpm, ret.qtsbxx]);
-      await wait(200);
+      wsData.push([ret.filename, ret.rq,, ret.xdr, ret.sjh, ret.shdz, ret.sfk, ret.ddh1, ret.xdsj, ret.spzj, ret.cpm, ret.qtsbxx]);
+
       fs.appendFileSync(processedResultFile, filePath + "\n");
       console.log('process file ok :', csvData, filePath, ret);
+
+
+      await wait(200);
 
 
     } catch (err) {
@@ -205,7 +207,7 @@ async function processAll() {
     };
   }
 
-  const wsName = imgdir.replace(/\./g, '').replace(/\//g, '_');
+  const wsName = fn;
 
 
   /* Create workbook */
